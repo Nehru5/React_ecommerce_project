5ac57bcc047e92dd3727dfb92c4559b12978c21e
Nehru raja <nehruraja9485@gmail.com>
2026-06-16 10:10:34 +0530

    Day 6

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -7,11 +7,45 @@ import { useNavigate, useParams } from 'react-router-dom'
 const ProductDetail = () => {
   let {id} = useParams()
   const [product, setProduct] = useState({})
+  const [quantity, setQuantity] = useState(1)
+  const [address, setAddress] = useState("")
+  let navigate = useNavigate()
   useEffect(()=>{
     axios.get(`http://localhost:3000/products/${id}`)
     .then(x=>{setProduct(x.data)})
     .catch(err=>console.log(err))
   },[])
+
+  function increment(){
+    setQuantity(quantity+1)
+  }
+  function decrement(){
+    if(quantity>1){
+      setQuantity(quantity-1)
+    }
+  }
+
+  function placeOrder(e){
+    e.preventDefault()
+    let orderData = {
+      userId:localStorage.getItem("id"),
+      productId:product.id,
+      productName:product.name,
+      productImage:product.image,
+      quantity:quantity,
+      price:product.price,
+      total:quantity*product.price,
+      date:new Date().toLocaleDateString(),
+      address:address,
+      status:"Delivered"
+    }
+    axios.post("http://localhost:3000/orders",orderData)
+    .then(()=>{
+      toast.success("Order placed🎉🎉")
+      navigate("/userdashboard")
+    })
+    .catch(err=>toast.error("Failed to place"))
+  }
   return (
     <>
       <Navbar2/>
@@ -19,7 +53,17 @@ const ProductDetail = () => {
       <img height={"200px"} src={product.image} alt="" />
       <h2>{product.description}</h2>
       <h2>{product.price}</h2>
-      <button>Order</button>
+      <div style={{marginLeft:"30px"}}>
+        <button onClick={decrement}>-</button>
+      <h2>{quantity}</h2>
+      <button onClick={increment}>+</button>
+      </div>
+
+      <form action="" onSubmit={placeOrder}>
+        <textarea placeholder='Enter Address' required value={address} onChange={(e)=>setAddress(e.target.value)}></textarea> <br />
+        <button>Place Order</button>
+      </form>
+
     </>
   )
 }
